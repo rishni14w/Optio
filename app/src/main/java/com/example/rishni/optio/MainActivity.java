d8df7b8f0827a03793b1aab8a06ac2f4c2ac883f
Rishni <Rishni@DESKTOP-7681NTT>
2018-05-25 17:11:56 +0530

    stresshealth and questionnaire UI

--- app/src/main/java/com/example/rishni/optio/MainActivity.java
+++ app/src/main/java/com/example/rishni/optio/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.rishni.optio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu,menu);
 
         return true;
+    }
+
+    public void clickedstressandhealth(View view)
+    {
+        if (view.getId()==R.id.StressHealthbtn)
+        {
+            Intent i= new Intent(MainActivity.this,StressAndHealth.class);
+            startActivity(i);
+        }
+
     }
 }
