361be3d92cbadd50c084087c62ff61946549540d
Atti <anuthara.attigala@my.sliit.lk>
2018-06-18 21:43:33 +0530

    Date picker added to the WeeklySteps.java

--- app/src/main/java/com/example/rishni/optio/WeeklySteps.java
+++ app/src/main/java/com/example/rishni/optio/WeeklySteps.java
@@ -1,13 +1,45 @@
 package com.example.rishni.optio;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
-public class WeeklySteps extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class WeeklySteps extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_steps);
+        Button buttonDate = findViewById(R.id.buttonPickDate);
+        buttonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker(view);
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        Calendar calendar = new GregorianCalendar(i,i1,i2);
+        setDate(calendar);
+    }
+    private void setDate(final Calendar calendar){
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        Toast.makeText(this,"Selected Date: "+dateFormat.format(calendar.getTime()),Toast.LENGTH_LONG).show();
+    }
+    public void datePicker(View view){
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.show(getFragmentManager(),"datePicker");
     }
 }
