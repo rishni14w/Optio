eccd31bb74bccb5b28a8778cf898fc6caaa422ff
Rishni <Rishni@DESKTOP-7681NTT>
2018-06-25 12:34:49 +0530

    vaccination add delete view and update and login disappear

--- app/src/main/java/com/example/rishni/optio/db.java
+++ app/src/main/java/com/example/rishni/optio/db.java
@@ -7,9 +7,11 @@ public class db {
     private  static String DB_NAME="optio";
     private static String COLLECTION_NAME="user";
     private static String COLLECTION_NAME_INJURY="injury";
+    private static String COLLECTION_NAME_VACCINATION="vaccination";
 
     public static String API_KEY="CoT0fJmLYRgJtCu2DpPG25PzvQVCtDks";
 
+    //injury
     public static String getAddressSingle_Injury(String oid)
     {
         String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_INJURY);
@@ -21,6 +23,24 @@ public class db {
     public static String getAddressAPI_Injury()
     {
         String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_INJURY);
+        StringBuilder stringBuilder=new StringBuilder(baseUrl);
+        stringBuilder.append("?apiKey="+API_KEY);
+        return stringBuilder.toString();
+    }
+
+
+    //vaccination
+    public static String getAddressSingle_Vaccination(String oid)
+    {
+        String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_VACCINATION);
+        StringBuilder stringBuilder=new StringBuilder(baseUrl);
+        stringBuilder.append("/"+oid+"?apiKey="+API_KEY);
+        return stringBuilder.toString();
+    }
+
+    public static String getAddressAPI_Vaccination()
+    {
+        String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_VACCINATION);
         StringBuilder stringBuilder=new StringBuilder(baseUrl);
         stringBuilder.append("?apiKey="+API_KEY);
         return stringBuilder.toString();
