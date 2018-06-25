8490f40de41ded6d02bccddcb62e260ab7a847b0
Rishni <Rishni@DESKTOP-7681NTT>
2018-06-25 18:41:48 +0530

    add function to insert sleep, heartrate and body composition details

--- app/src/main/java/com/example/rishni/optio/db.java
+++ app/src/main/java/com/example/rishni/optio/db.java
@@ -5,9 +5,11 @@ import com.example.rishni.optio.model.Injury;
 
 public class db {
     private  static String DB_NAME="optio";
-    private static String COLLECTION_NAME="user";
+    private static String COLLECTION_NAME_BODYCOMPOSITION="bodycomposition";
     private static String COLLECTION_NAME_INJURY="injury";
     private static String COLLECTION_NAME_VACCINATION="vaccination";
+    private static String COLLECTION_NAME_SLEEP="sleep";
+    private static String COLLECTION_NAME_HEARTRATE="heartrate";
 
     public static String API_KEY="CoT0fJmLYRgJtCu2DpPG25PzvQVCtDks";
 
@@ -41,6 +43,33 @@ public class db {
     public static String getAddressAPI_Vaccination()
     {
         String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_VACCINATION);
+        StringBuilder stringBuilder=new StringBuilder(baseUrl);
+        stringBuilder.append("?apiKey="+API_KEY);
+        return stringBuilder.toString();
+    }
+
+    //Sleep
+    public static String getAddressAPI_Sleep()
+    {
+        String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_SLEEP);
+        StringBuilder stringBuilder=new StringBuilder(baseUrl);
+        stringBuilder.append("?apiKey="+API_KEY);
+        return stringBuilder.toString();
+    }
+
+    //Heartrate
+    public static String getAddressAPI_Heartrate()
+    {
+        String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_HEARTRATE);
+        StringBuilder stringBuilder=new StringBuilder(baseUrl);
+        stringBuilder.append("?apiKey="+API_KEY);
+        return stringBuilder.toString();
+    }
+
+    //bodycomposition
+    public static String getAddressAPI_BodyComposition()
+    {
+        String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_BODYCOMPOSITION);
         StringBuilder stringBuilder=new StringBuilder(baseUrl);
         stringBuilder.append("?apiKey="+API_KEY);
         return stringBuilder.toString();
