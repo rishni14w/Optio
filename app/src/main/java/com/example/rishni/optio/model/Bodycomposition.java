34c3e97af9b6e949708682e0705e00cdc2e259a4
Rishni <Rishni@DESKTOP-7681NTT>
2018-06-25 18:39:43 +0530

    add function to insert sleep, heartrate and body composition details

--- app/src/main/java/com/example/rishni/optio/model/Bodycomposition.java
+++ app/src/main/java/com/example/rishni/optio/model/Bodycomposition.java
@@ -1,11 +1,11 @@
 package com.example.rishni.optio.model;
 
-public class User {
+public class Bodycomposition {
     private  Id _id;
-    private String user;
     private String weight;
     private String waist;
     private String hip;
+    private String date;
 
     public Id get_id() {
         return _id;
@@ -13,14 +13,6 @@ public class User {
 
     public void set_id(Id _id) {
         this._id = _id;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
     }
 
     public String getWeight() {
@@ -45,5 +37,13 @@ public class User {
 
     public void setHip(String hip) {
         this.hip = hip;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
