ff25a6820f422b504ffc25b4fa1edda4c521870e
Rishni <Rishni@DESKTOP-7681NTT>
2018-06-27 21:18:58 +0530

    check uncheck none and submit illness and injury questionnaire

--- app/src/main/java/com/example/rishni/optio/db.java
+++ app/src/main/java/com/example/rishni/optio/db.java
@@ -10,6 +10,7 @@ public class db {
     private static String COLLECTION_NAME_VACCINATION="vaccination";
     private static String COLLECTION_NAME_SLEEP="sleep";
     private static String COLLECTION_NAME_HEARTRATE="heartrate";
+    private static String COLLECTION_NAME_ILLNESSINJURY="illnessinjury";
 
     public static String API_KEY="CoT0fJmLYRgJtCu2DpPG25PzvQVCtDks";
 
@@ -70,6 +71,15 @@ public class db {
     public static String getAddressAPI_BodyComposition()
     {
         String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_BODYCOMPOSITION);
+        StringBuilder stringBuilder=new StringBuilder(baseUrl);
+        stringBuilder.append("?apiKey="+API_KEY);
+        return stringBuilder.toString();
+    }
+
+    //illness and injury
+    public static String getAddressAPI_IllnessInjury()
+    {
+        String baseUrl=String.format("https://api.mlab.com/api/1/databases/%s/collections/%s",DB_NAME,COLLECTION_NAME_ILLNESSINJURY);
         StringBuilder stringBuilder=new StringBuilder(baseUrl);
         stringBuilder.append("?apiKey="+API_KEY);
         return stringBuilder.toString();
