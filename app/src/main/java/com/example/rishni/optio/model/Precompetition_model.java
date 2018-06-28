d34c6622942a29f36be6f5100b96b8663b17582f
Rishni <Rishni@DESKTOP-7681NTT>
2018-06-28 20:30:51 +0530

    precompetition submit

---
+++ app/src/main/java/com/example/rishni/optio/model/Precompetition_model.java
@@ -0,0 +1,157 @@
+package com.example.rishni.optio.model;
+
+public class Precompetition_model {
+    private  Id _id;
+    private String nervous;
+    private String continuous;
+    private String eve;
+    private String before;
+    private String entering;
+    private String firstSet;
+    private String strongOpponent;
+    private String strongOpponent_reason;
+    private String lower_rank;
+    private String insecure;
+    private String loosing;
+    private String first_round;
+    private String upcoming;
+    private String upcoming_reason;
+    private String input;
+    private String rude;
+
+    public Id get_id() {
+        return _id;
+    }
+
+    public void set_id(Id _id) {
+        this._id = _id;
+    }
+
+    public String getNervous() {
+        return nervous;
+    }
+
+    public void setNervous(String nervous) {
+        this.nervous = nervous;
+    }
+
+    public String getContinuous() {
+        return continuous;
+    }
+
+    public void setContinuous(String continuous) {
+        this.continuous = continuous;
+    }
+
+    public String getEve() {
+        return eve;
+    }
+
+    public void setEve(String eve) {
+        this.eve = eve;
+    }
+
+    public String getBefore() {
+        return before;
+    }
+
+    public void setBefore(String before) {
+        this.before = before;
+    }
+
+    public String getEntering() {
+        return entering;
+    }
+
+    public void setEntering(String entering) {
+        this.entering = entering;
+    }
+
+    public String getFirstSet() {
+        return firstSet;
+    }
+
+    public void setFirstSet(String firstSet) {
+        this.firstSet = firstSet;
+    }
+
+    public String getStrongOpponent() {
+        return strongOpponent;
+    }
+
+    public void setStrongOpponent(String strongOpponent) {
+        this.strongOpponent = strongOpponent;
+    }
+
+    public String getStrongOpponent_reason() {
+        return strongOpponent_reason;
+    }
+
+    public void setStrongOpponent_reason(String strongOpponent_reason) {
+        this.strongOpponent_reason = strongOpponent_reason;
+    }
+
+    public String getLower_rank() {
+        return lower_rank;
+    }
+
+    public void setLower_rank(String lower_rank) {
+        this.lower_rank = lower_rank;
+    }
+
+    public String getInsecure() {
+        return insecure;
+    }
+
+    public void setInsecure(String insecure) {
+        this.insecure = insecure;
+    }
+
+    public String getLoosing() {
+        return loosing;
+    }
+
+    public void setLoosing(String loosing) {
+        this.loosing = loosing;
+    }
+
+    public String getFirst_round() {
+        return first_round;
+    }
+
+    public void setFirst_round(String first_round) {
+        this.first_round = first_round;
+    }
+
+    public String getUpcoming() {
+        return upcoming;
+    }
+
+    public void setUpcoming(String upcoming) {
+        this.upcoming = upcoming;
+    }
+
+    public String getUpcoming_reason() {
+        return upcoming_reason;
+    }
+
+    public void setUpcoming_reason(String upcoming_reason) {
+        this.upcoming_reason = upcoming_reason;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getRude() {
+        return rude;
+    }
+
+    public void setRude(String rude) {
+        this.rude = rude;
+    }
+}
