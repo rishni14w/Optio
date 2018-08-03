ef64fba37c63308b23b564e573b7dc3bc5c84256
Rishni <Rishni@DESKTOP-7681NTT>
2018-08-03 11:37:12 +0530

    sleep modification

--- app/src/main/java/com/example/rishni/optio/Sleep.java
+++ app/src/main/java/com/example/rishni/optio/Sleep.java
@@ -1,21 +1,44 @@
 package com.example.rishni.optio;
 
-import android.annotation.TargetApi;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Sleep extends AppCompatActivity {
+
+    String ServerURL = "https://murmuring-cove-69371.herokuapp.com/sleep";
 
     private EditText editText_Hour;
     private EditText editText_Min;
+
+    String date;
+    String nic;
+    String duration;
+
+    Validation validation=new Validation();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +47,9 @@ public class Sleep extends AppCompatActivity {
 
         editText_Hour=findViewById(R.id.digit_hr);
         editText_Min=findViewById(R.id.digit_min);
+
+        SharedPreferences sharedPref=getApplicationContext().getSharedPreferences("AthletePref",0);
+        nic=sharedPref.getString("AthleteNic","");
     }
 
     @Override
@@ -39,9 +65,43 @@ public class Sleep extends AppCompatActivity {
         {
             String newHour=editText_Hour.getText().toString();
             String newMin=editText_Min.getText().toString();
-            String duration=newHour+" hr and "+newMin+" min";
 
-            new PostData(duration).execute(db.getAddressAPI_Sleep());
+            Boolean result=validation.isEmpty(newHour,newMin);
+            if(result.equals(false))
+            {
+                Boolean result2=validation.isGreaterthan(newHour,newMin);
+                if(result2.equals(false))
+                {
+                    duration=newHour+"hr-"+newMin+"min";
+
+                    Date currentTime= Calendar.getInstance().getTime();
+                    date=currentTime.toString();
+                    //new PostData(duration).execute(db.getAddressAPI_Sleep());
+
+                    new SendData().execute();
+                }
+                else
+                {
+                    AlertDialog dialog=new AlertDialog.Builder(this).setTitle("Error").setMessage("Hours should be less than or equal to 24 and minutes should be less than or equal to 60").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    }).create();
+                    dialog.show();
+                }
+
+            }
+            else
+            {
+                AlertDialog dialog=new AlertDialog.Builder(this).setTitle("Error").setMessage("Both hours and minutes are required").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).create();
+                dialog.show();
+            }
 
         }
         else
@@ -55,8 +115,8 @@ public class Sleep extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //function to add new user
-    class PostData extends AsyncTask<String,String,String> {
+    //function to add new sleep duration
+    /**class PostData extends AsyncTask<String,String,String> {
         String duration;
 
         public PostData(String duration) {
@@ -93,6 +153,74 @@ public class Sleep extends AppCompatActivity {
             finish();
         }
     }
+**/
+    //post sleep details
+    class SendData extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            doPost();
+            return null;
+        }
+
+        protected void doPost() {
+            try {
+                URL url = new URL(ServerURL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+                conn.connect();
+
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("sleepDuration", duration);
+                jsonParam.put("nic", nic);
+                jsonParam.put("date", date);
+
+
+                conn.getOutputStream();
+                try {
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                Log.i("MSG" , conn.getResponseMessage());
+
+                conn.disconnect();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        protected void onPostExecute(Object object) {
+            super.onPostExecute(object);
+            toastMessage("Successfully saved");
+            Intent intent = new Intent(Sleep.this, StressAndHealth.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 
     private void toastMessage(String message)
     {
