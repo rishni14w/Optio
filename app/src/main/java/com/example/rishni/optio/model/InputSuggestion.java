08d1d6aa0c5c61c1afac8591ea1d1da1800d88b1
Rishni <Rishni@DESKTOP-7681NTT>
2018-09-25 00:51:36 +0530

    stress question

---
+++ app/src/main/java/com/example/rishni/optio/model/InputSuggestion.java
@@ -0,0 +1,40 @@
+package com.example.rishni.optio.model;
+
+public class InputSuggestion {
+    private String id;
+    private String nic;
+    private String input;
+    private String suggestion;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getSuggestion() {
+        return suggestion;
+    }
+
+    public void setSuggestion(String suggestion) {
+        this.suggestion = suggestion;
+    }
+}
